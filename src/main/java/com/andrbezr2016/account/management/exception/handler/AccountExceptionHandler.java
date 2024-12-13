4c0591f675d5fbc1fc580114e2a32fab62c64b2b
Bezrukov Andrey <andrbezr2016@yandex.ru>
2024-12-13 15:46:57 +0300

    add some logs

--- src/main/java/com/andrbezr2016/account/management/exception/handler/AccountExceptionHandler.java
+++ src/main/java/com/andrbezr2016/account/management/exception/handler/AccountExceptionHandler.java
@@ -2,6 +2,7 @@ package com.andrbezr2016.account.management.exception.handler;
 
 import jakarta.validation.ConstraintViolation;
 import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
@@ -10,17 +11,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.stream.Collectors;
 
+@Slf4j
 @RestControllerAdvice
 public class AccountExceptionHandler {
 
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<String> handleConstraintViolationException(ConstraintViolationException exception) {
         String message = exception.getConstraintViolations().stream().map(ConstraintViolation::getMessage).collect(Collectors.joining(", "));
+        log.error(message);
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(message);
     }
 
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<String> handleHttpMessageNotReadableException(HttpMessageNotReadableException exception) {
+        log.error("Wrong input format");
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Wrong input format");
     }
 }
